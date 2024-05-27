1dabeb9f234adce4f197dc8214e2d901116a2006
x <y>
2024-05-27 17:20:29 +0000

    Fix potential data races

--- .github/workflows/buildTest.bat
+++ .github/workflows/buildTest.bat
@@ -2,4 +2,4 @@
 
 MD build 2>NUL
 
-"%1" ".github/workflows/test.mpl" -D DEBUG=TRUE -D MPLC=\"%1\" -I "" -I windows -ndebug -o build/test.ll && clang -O0 -o build/test.exe atomic.ll build/test.ll
+"%1" ".github/workflows/test.mpl" -D DEBUG=TRUE -D MPLC=\"%1\" -D SL_MEMORY_MAX_CACHED_SIZE=0nx -I "" -I windows -ndebug -o build/test.ll && clang -O0 -o build/test.exe atomic.ll build/test.ll
