cdcd79ea63e78b84c6d5a8f700067c8a46f50e95
x <y>
2025-09-06 18:48:38 +0000

    Align test.bat and test.sh with each other

--- test.bat
+++ test.bat
@@ -6,5 +6,5 @@ CHCP 65001
 
 REM TODO: Do the first two tasks in parallel
 "%1" .github/workflows/test.mpl -D DEBUG=TRUE -D MPLC=\"%1\" -D PLATFORM=\"windows\" -I "" -I windows -ndebug -o out/test.ll && clang -O0 -o out/test.exe atomic.ll out/test.ll^
-  && "%1" .github/workflows/sequentialTasks.mpl -D DEBUG=TRUE -D MPLC=\"%1\" -D PLATFORM=\"windows\" -I "" -I windows -ndebug -o out/sequentialTasks.ll && clang -O0 -o out/sequentialTasks.exe atomic.ll out/sequentialTasks.ll^
+  && "%1" .github/workflows/sequentialTasks.mpl -D DEBUG=TRUE -I "" -I windows -ndebug -o out/sequentialTasks.ll && clang -O0 -o out/sequentialTasks.exe atomic.ll out/sequentialTasks.ll^
   && "out/test"
