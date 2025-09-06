ca8d55d8476a9074655043b36eb648ec49973faf
x <y>
2025-09-06 17:46:28 +0000

    Rename processTests.mpl to test.mpl

--- test.bat
+++ test.bat
@@ -5,6 +5,6 @@ MD          out 2>NUL
 CHCP 65001
 
 REM TODO: Do the first two tasks in parallel
-"%1" .github/workflows/processTests.mpl -D DEBUG=TRUE -D MPLC=\"%1\" -D PLATFORM=\"windows\" -I "" -I windows -ndebug -o out/processTests.ll && clang -O0 -o out/processTests.exe atomic.ll out/processTests.ll^
+"%1" .github/workflows/test.mpl -D DEBUG=TRUE -D MPLC=\"%1\" -D PLATFORM=\"windows\" -I "" -I windows -ndebug -o out/test.ll && clang -O0 -o out/test.exe atomic.ll out/test.ll^
   && "%1" .github/workflows/sequentialTasks.mpl -D DEBUG=TRUE -D MPLC=\"%1\" -D PLATFORM=\"windows\" -I "" -I windows -ndebug -o out/sequentialTasks.ll && clang -O0 -o out/sequentialTasks.exe atomic.ll out/sequentialTasks.ll^
-  && "out/processTests"
+  && "out/test"
